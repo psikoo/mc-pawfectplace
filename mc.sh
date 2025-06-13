da74492cc6e3ea5c814b34706802a89bb9f3c4d8
psikoo <91342186+psikoo@users.noreply.github.com>
2025-06-13 15:54:01 +0200

    a

--- mc.sh
+++ mc.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+# Setup
+apk update && apk add libstdc++ && apk add openjdk21-jre && apk add bash && apk add wget && apk add jq
+mkdir /papermc
 cd papermc
 
+# Env
 PROJECT="paper"
 #LATEST_VERSION=$(curl -s https://api.papermc.io/v2/projects/${PROJECT} | jq -r '.versions[-1]')
 MC_VERSION="1.21.4"
 LATEST_BUILD=$(curl -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MC_VERSION}/builds | jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')
 
+# Download jar
 if [ "$LATEST_BUILD" != "null" ]; then
   JAR_NAME=${PROJECT}-${MC_VERSION}-${LATEST_BUILD}.jar
   PAPERMC_URL="https://api.papermc.io/v2/projects/${PROJECT}/versions/${MC_VERSION}/builds/${LATEST_BUILD}/downloads/${JAR_NAME}"
-
-  # Download the latest Paper version
   curl $PAPERMC_URL > paper.jar
   echo "> Jar download completed"
 else
   echo "No stable build for version $MC_VERSION found :("
 fi
 
+# Set eula
 echo "eula=true" > eula.txt
 
+# run server
 JAVA_OPTS="-Xms8192M -Xmx8192M"
 exec java -server $JAVA_OPTS -jar "paper.jar" nogui
