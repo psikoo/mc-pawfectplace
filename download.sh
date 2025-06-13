879d7165023c0cd86ea31db59aa1ed9e07ec8455
psikoo <91342186+psikoo@users.noreply.github.com>
2025-06-13 20:58:44 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,3 +2,4 @@
 curl https://download.geysermc.org/v2/projects/geyser/versions/2.7.1/builds/851/downloads/spigot > ./plugins/Geyser-Spigot.jar
 curl https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.3.2/PAPER/ViaVersion-5.3.2.jar > ./plugins/ViaVersion-5.3.2.jar
 curl https://hangarcdn.papermc.io/plugins/pop4959/Chunky/versions/1.4.36/PAPER/Chunky-Bukkit-1.4.36.jar > ./plugins/Chunky-Bukkit-1.4.36.jar
+curl https://hangarcdn.papermc.io/plugins/SRTeam/SkinsRestorer/versions/15.6.5/PAPER/SkinsRestorer.jar > ./plugins/SkinsRestorer.jar
