75a1992686c7a7fecba751153caaa12da465ac5c
psikoo <91342186+psikoo@users.noreply.github.com>
2025-06-13 16:37:02 +0200

    Update mc.sh

--- mc.sh
+++ mc.sh
@@ -4,15 +4,15 @@ cd papermc
 
 # Env
 PROJECT="paper"
-#LATEST_VERSION=$(wget -qO -s https://api.papermc.io/v2/projects/${PROJECT} | jq -r '.versions[-1]')
+#LATEST_VERSION=$(wget -O -s https://api.papermc.io/v2/projects/${PROJECT} | jq -r '.versions[-1]')
 MC_VERSION="1.21.4"
-LATEST_BUILD=$(wget -qO -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MC_VERSION}/builds | jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')
+LATEST_BUILD=$(wget -O -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MC_VERSION}/builds | jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')
 
 # Download jar
 if [ "$LATEST_BUILD" != "null" ]; then
   JAR_NAME=${PROJECT}-${MC_VERSION}-${LATEST_BUILD}.jar
   PAPERMC_URL="https://api.papermc.io/v2/projects/${PROJECT}/versions/${MC_VERSION}/builds/${LATEST_BUILD}/downloads/${JAR_NAME}"
-  wget -q $PAPERMC_URL > paper.jar
+  wget $PAPERMC_URL > paper.jar
   echo "> Jar download completed"
 else
   echo "No stable build for version $MC_VERSION found :("
