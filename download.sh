a3e58892ed90eb182131c63a3f1c0b52e65abfd1
psikoo <91342186+psikoo@users.noreply.github.com>
2025-06-13 20:09:01 +0200

    a

--- download.sh
+++ download.sh
@@ -2,4 +2,3 @@
 curl https://download.geysermc.org/v2/projects/geyser/versions/2.7.1/builds/851/downloads/spigot > ./plugins/Geyser-Spigot.jar
 curl https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.3.2/PAPER/ViaVersion-5.3.2.jar > ./plugins/ViaVersion-5.3.2.jar
 curl https://hangarcdn.papermc.io/plugins/pop4959/Chunky/versions/1.4.36/PAPER/Chunky-Bukkit-1.4.36.jar > ./plugins/Chunky-Bukkit-1.4.36.jar
-curl https://github.com/plan-player-analytics/Plan/releases/download/5.6.2959/Plan-5.6-build-2959.jar > ./plugins/Plan-5.6-build-2965.jar
